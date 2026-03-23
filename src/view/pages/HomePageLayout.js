071898716d2f62d2d8d908101522abf41ee85944
nuuuwan <nuuuwan@gmail.com>
2026-03-23 13:57:37 +0530

    feat: add message to display number of public datasets in HomePageLayout

--- src/view/pages/HomePageLayout.js
+++ src/view/pages/HomePageLayout.js
@@ -44,6 +44,9 @@ export default function HomePageLayout({
       <header className="top-nav">
         <div>
           <h1>Sri Lanka Time Series</h1>
+          {metadata.length > 0 && (
+            <p>Search and visualize {metadata.length.toLocaleString()} public datasets.</p>
+          )}
         </div>
         <div className="top-nav-right"></div>
       </header>
