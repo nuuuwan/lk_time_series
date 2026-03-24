2f2885da989ebc22befb5991e8691f1d42352193
nuuuwan <nuuuwan@gmail.com>
2026-03-24 10:57:04 +0530

    fix: remove unused formatDate import from DatasetDetails component

--- src/view/moles/DatasetDetails.js
+++ src/view/moles/DatasetDetails.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { buildDatasetRawUrl } from "../../nonview/core/datasetApi";
 import {
-  formatDate,
   formatNumber,
   formatDateByFrequency,
 } from "../../nonview/core/timeSeriesUtils";
